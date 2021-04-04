d9fa6085b39bf572aae1257878db9fbda4cee57a
jalan116 <jalan116@stu.southtexascollege.edu>
2021-04-04 01:13:07 -0500

    Added more robust configuration

---
+++ src/helpers/errors.js
@@ -0,0 +1,21 @@
+import logger from './logger'
+
+
+const notFound = (req, res, next) => {
+    const error = new Error('Not Found - ${req.originalURL}')
+    res.status(404)
+    next(error)
+}
+
+export const errorHandler = (error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    logger.error({
+      message: error.message,
+      stack: error.stack,
+    })
+    res.status(statusCode)
+    res.json({
+      message: error.message,
+      stack: process.env.NODE_ENV === 'production' ? '💩' : error.stack,
+    })
+}
